ed75ae6d2dc99b6bd50d110d502f256a7263a351
Amariah Kamau <110414493+AmariahAK@users.noreply.github.com>
2025-05-10 16:12:34 +0300

    fixing some stuff

--- api/consumers.py
+++ api/consumers.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.contrib.auth.models import User
