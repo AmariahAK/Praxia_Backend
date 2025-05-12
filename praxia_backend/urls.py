343a27f888ea6438c39f02f93a912dbf093e95ec
Amariah Kamau <110414493+AmariahAK@users.noreply.github.com>
2025-05-12 11:02:53 +0300

    removed unnecesary comments

--- praxia_backend/urls.py
+++ praxia_backend/urls.py
@@ -6,6 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls.urls')),
-    # Add Prometheus metrics endpoint
     path('', include('django_prometheus.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
