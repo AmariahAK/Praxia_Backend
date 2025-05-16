7d81e295b9e73956aa388cad670828042cfd10eb
Amariah Kamau <110414493+AmariahAK@users.noreply.github.com>
2025-05-16 22:22:55 +0300

    fixing more issues

--- praxia_backend/asgi.py
+++ praxia_backend/asgi.py
@@ -1,19 +1,17 @@
 """
 ASGI config for praxia_backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'praxia_backend.settings')
+django.setup()  # Set up Django before importing models
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from api.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'praxia_backend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
