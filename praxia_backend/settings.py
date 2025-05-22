8271868f5272033e089009dc02bce09ff3f12a9f
Amariah Kamau <110414493+AmariahAK@users.noreply.github.com>
2025-05-22 14:52:34 +0300

    fixing some minor bugs

--- praxia_backend/settings.py
+++ praxia_backend/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'praxia_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'api'), 
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
